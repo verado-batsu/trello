7760b5799c1239667d3708dfca5bd863e575c363
Vladyslav <kupchuk.vlad@gmail.com>
2024-01-19 20:43:22 +0200

    ADDED: drag and drop of cards

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import './App.scss';
+
+import { cardData } from './data/faqData';
+
+function App() {
+    const [cardList, setCardList] = useState(cardData);
+    const [currentCard, setCurrentCard] = useState(null);
+
+    function dragStartHandler(e, card) {
+        setCurrentCard(card);
+    }
+
+    function dragEndHandler(e) {
+        e.target.style.background = 'white';
+    }
+
+    function dragOverHandler(e) {
+        e.preventDefault();
+        e.target.style.background = 'lightgrey';
+    }
+
+    function dropHandler(e, card) {
+        e.preventDefault();
+        e.target.style.background = 'white';
+        setCardList(
+            cardList.map(c => {
+                if (c.id === card.id) {
+                    return { ...c, order: currentCard.order };
+                }
+                if (c.id === currentCard.id) {
+                    return { ...c, order: card.order };
+                }
+                return c;
+            })
+        );
+    }
+
+    function compareCards(a, b) {
+        if (a.order > b.order) {
+            return 1;
+        } else if (a.order < b.order) {
+            return -1;
+        }
+    }
+
+    return (
+        <div className="app">
+            {cardList.sort(compareCards).map(card => (
+                <li
+                    onDragStart={e => dragStartHandler(e, card)}
+                    onDragLeave={e => dragEndHandler(e)}
+                    onDragEnd={e => dragEndHandler(e)}
+                    onDragOver={e => dragOverHandler(e)}
+                    onDrop={e => dropHandler(e, card)}
+                    draggable={true}
+                    className="card"
+                    key={card.id}
+                >
+                    {card.text}
+                </li>
+            ))}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/data/faqData.js
@@ -0,0 +1,22 @@
+export const cardData = [
+	{
+		id: 1,
+		order: 3,
+		text: 'Card 3'
+	},
+	{
+		id: 2,
+		order: 1,
+		text: 'Card 1'
+	},
+	{
+		id: 3,
+		order: 2,
+		text: 'Card 2'
+	},
+	{
+		id: 4,
+		order: 4,
+		text: 'Card 4'
+	},
+]
